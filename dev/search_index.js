fad5847249a299ef5cb6415e1be216ba696361c4
Documenter.jl <documenter@juliadocs.github.io>
2023-10-25 04:39:03 +0000

    build based on e1ca9de

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TimeseriesFeatures","category":"page"},{"location":"#TimeseriesFeatures","page":"Home","title":"TimeseriesFeatures","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TimeseriesFeatures.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TimeseriesFeatures]","category":"page"}]
+}
